289e36f1daf01b12b79a32e3be0c203da640e332
Joan Cortes Torandell <160049122+JoanCort04@users.noreply.github.com>
2025-01-15 16:49:32 +0100

    Creat el nou.php a partir de la branca develop

--- Nou.php
+++ Nou.php
@@ -3,5 +3,9 @@
 /* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+<<<<<<< Updated upstream
  */
 
+=======
+ */
+>>>>>>> Stashed changes
